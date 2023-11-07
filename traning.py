3a17ba5d34000bbf1114496e6e7185636a73028c
a5642 <564298339@qq.com>
2023-11-07 17:40:29 +0800

    1.48 prepare for dist2 algo

--- traning.py
+++ traning.py
@@ -115,7 +115,7 @@ class DE:
         self.time_recorder.append(time())
         self.training_log.append([len(self.time_recorder),1/self.eval(xk)])
 
-        print(f'time cost {self.time_recorder[-1] - self.time_recorder[-2]} Current solution: {xk}, ratio={1/self.eval(xk)} , Convergence: {convergence}')
+        print(f'time cost {self.time_recorder[-1] - self.time_recorder[-2]} Current solution: {list(xk)}, ratio={1/self.eval(xk)} , Convergence: {convergence}')
 
 
 def solution_draw(solution:BinPacking2DAlgo.Algo,text=""):
