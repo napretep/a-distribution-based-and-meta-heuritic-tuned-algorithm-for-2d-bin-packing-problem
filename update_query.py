45253973145e834be9d3bb75b1465d7a366b6b40
a5642 <564298339@qq.com>
2023-11-10 21:09:36 +0800

    1.79 轮询

--- update_query.py
+++ update_query.py
@@ -12,7 +12,7 @@ import os
 import subprocess
 
 # 配置参数
-ACCESS_TOKEN ="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" # os.environ.get("GITHUB_TOKEN")  # 从环境变量获取令牌
+ACCESS_TOKEN =os.environ.get("GITHUB_TOKEN")  # 从环境变量获取令牌
 REPO = "napretep/a-distribution-based-and-meta-heuritic-tuned-algorithm-for-2d-bin-packing-problem"  # GitHub 仓库，格式为'用户名/仓库名'
 BRANCH = "main"  # 目标分支
 POLL_INTERVAL = 8  # 轮询间隔（秒）
