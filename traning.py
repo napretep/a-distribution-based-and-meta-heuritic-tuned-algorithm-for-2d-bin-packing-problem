dda8241de28b34b98ea5d60f928f6de561f40de0
a5642 <564298339@qq.com>
2023-11-10 08:00:22 +0800

    1.67 dist2 compare

--- traning.py
+++ traning.py
@@ -195,7 +195,7 @@ class Training:
         result = []
         for data, name in self.data_set:
             start_time2 = time()
-            d = DE(data, name, random_ratio=(0, 0.3) if self.training_type=="noised" else None, max_iter=1000, data_sample_scale=1500, eval_selector="single")
+            d = DE(data, name, random_ratio=(0, 0.3) if self.training_type=="noised" else None, max_iter=500, data_sample_scale=1500, eval_selector="single")
             x, fun, log = d.run()
             result.append([name, x, 1 / fun, f"训练用时(秒):{time() - start_time2}"])
             np.save(f"{self.training_type}_Dist_{name}_{fun}__{round(time())}.npy", np.array(x))
