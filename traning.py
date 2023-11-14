653d563942b5e2c1535f59b9e810d1752134830d
napretep <564298339@qq.com>
2023-11-14 22:06:42 +0800

    1.125

--- traning.py
+++ traning.py
@@ -9,6 +9,7 @@ __time__ = '2023/10/19 8:31'
 import functools
 from time import time
 
+
 import numpy as np
 
 from algorithm import Skyline, Distribution, MaxRect
@@ -16,53 +17,11 @@ from algorithm.distribution_based import ScoringSys
 from constant import *
 from visualizing.draw_plan import standard_draw_plan
 import BinPacking2DAlgo
-from multiprocessing import Pool
-#from memory_profiler import profile
+import multiprocessing
+
+from memory_profiler import profile
 # from pympler import asizeof as variable_len
 import gc,sys
-"""
-确定性
-全部训练完成时间(秒): 57250.41469120979 500次
-[['production_data1', array([ 27.57934813,  23.29018577,  33.43375348,  18.89843672,
-       -18.28887118,   0.36416545,  38.55297982,   1.58717868,
-       -12.72023321,  -4.78548915,   1.24706308, -30.0087219 ,
-       -26.44875766,  19.04054086, -39.76115475,   2.18626198,
-       -29.64918256, -14.72861541,  23.58872823,  26.29482364,
-       -10.93733512,   2.4618385 ,   7.3259813 ,  19.91113574]), 0.9599604740738868, '训练用时(秒):11248.336431026459'], 
-['production_data2', array([ 28.12061657,  -8.0430912 ,  22.58183676, -36.278031  ,
-       -18.67314595,   0.14616366,  22.46584206, -35.59192484,
-         1.45843571,   2.81054814,  -4.70562306,   7.44944437,
-       -11.04635553, -25.21184856,  33.64858665,   1.43668068,
-         1.38881597,  -2.31121838,  37.72415834,   9.3078541 ,
-         8.54222983,   2.6429937 ,  -3.17287881,  -9.44685875]), 0.9649471915685213, '训练用时(秒):12825.52998805046'], 
-['random_data', array([ 25.5435356 ,   9.25573678,   7.33452141,  11.87821915,
-       -18.15776172,  -0.69890311,  32.35026717,  20.43240378,
-         7.59892088,   5.14509784,  -2.94299651,   2.40380363,
-       -34.66379054,   8.21110542, -37.56377915, -10.16772923,
-       -30.83058312,  26.36755633,  36.43783522, -13.96861355,
-        23.04090018,   2.31979539,  -7.09244668,  -0.84345639]), 0.9345276623964309, '训练用时(秒):33176.54827213287']]
-随机性
-全部训练完成时间(秒): 30622.85742497444 250次
-[['production_data1', array([ 32.5297365 ,  17.42349883,  31.07130181, -27.08239102,
-       -16.1294125 ,   0.55513815,  24.09125474,  18.33520099,
-       -14.46151256,   0.85308145,  -0.98344585, -31.19569029,
-       -21.8196573 , -30.40856389,   0.17618179,  -3.6816786 ,
-        28.74556118,   8.17828654,  26.91246714,   5.98856374,
-       -17.47834592, -31.059624  , -23.15718183,  27.40120483]), 0.9553601099894597, '训练用时(秒):6811.973150014877'], 
-['production_data2', array([ 32.81051893,  27.71859658, -18.12926271,  31.47141733,
-        11.15234478,   0.98452451,  30.20495797, -13.62208354,
-        14.46456117,   0.35245309,   2.57142432, -17.99945398,
-       -29.75812519,  24.37060543, -13.10154752,  -6.09719204,
-         7.50557726,  -8.27136646,  36.6475308 ,   3.24912781,
-        -4.3851668 ,   2.2489736 ,  35.10086676,   6.805312  ]), 0.9656827893012611, '训练用时(秒):7960.055242538452'], 
-['random_data', array([ 20.53944317, -14.50081467, -13.72178025, -36.98244615,
-        38.81836636,   0.55734865,   4.25591023, -23.77335997,
-         4.96419603,   4.04618501,   5.64926788, -34.76708757,
-       -32.87163442, -36.30439092,   8.58333456,  36.94052644,
-         9.05199327, -26.73226726,   4.89877997, -39.19794429,
-         5.8054671 ,  12.25104461,  14.58953578,  14.81294095]), 0.9462488553462883, '训练用时(秒):15850.829032421112']]
-
-"""
 
 class EvalSelect:
     Multi="Multi"
@@ -70,14 +29,16 @@ class EvalSelect:
 
 
 @dataclasses.dataclass
-class AlgoArgs:
-    data: np.ndarray
-    params: np.ndarray
+class SingleAlgoArgs:
+    weights: np.ndarray
     algo_name: str
+    data_sample_scale: int
+    data_set_name: str
+    random_ratio: tuple | None
 
 @dataclasses.dataclass
 class DE_MultiArgs:
-    data_set:np.ndarray
+    data_set_name:str
     idvl_idx:int
     pop:np.ndarray
     pop_size:int
@@ -92,6 +53,12 @@ class DE_MultiArgs:
     eval_run_count:int
     algo_name:str
     pop_fitness:np.ndarray
+
+    def single_run(self):
+        pass
+
+    def multi_run(self):
+        pass
 
 def packing_log_vector_to_obj(packinglog: "List[List[List[List[float]]]]"):
     solution = []
@@ -123,28 +90,49 @@ def show_memory():
     for frame,name,obj, size in sorted(objects_list, key=functools.cmp_to_key(cmp))[:10]:
         print(f"""FRAMEat:{frame} NAME:{name} ID:{id(obj)}, TYPE: {type(obj)} {f'ELEMENT_COUNT:{len(obj)}' if type(obj)==list or type(obj)==dict else ''}  objSIZE: {size/1024/1024:.2f}MB {str(obj)[:100]}""")
 
-class DE_EVAL_for_run_v1:
-    def __init__(self,data_set,data_sample_scale,random_ratio,algo_name,eval_run_count):
-        self.data_set=data_set
-        self.data_sample_scale = data_sample_scale
-        self.random_ratio = random_ratio
-        self.algo_name = algo_name
-        self.eval_run_count = eval_run_count
 
 
-    def gen_data(self):
-        input_data =[ kde_sample(self.data_set,self.data_sample_scale) if self.random_ratio is None else random_mix(kde_sample(self.data_set,self.data_sample_scale)[:,1:],self.random_ratio)
-                       for k in range(self.eval_run_count)]
-        return input_data
-    def eval(self,idvl_param):
-        results = BinPacking2DAlgo.multi_run(self.gen_data(), MATERIAL_SIZE, parameter_input_array=idvl_param,
-                                             algo_type=self.algo_name, run_count=self.eval_run_count)
-        results = np.array(results)
-        return np.mean(1 / results)
+def sub_process_idvl_eval(arg: SingleAlgoArgs):
 
+    input_data = kde_sample(data_sets[arg.data_set_name],arg.data_sample_scale)
+    if arg.random_ratio is not None:
+        input_data = random_mix(input_data[:,1:],arg.random_ratio)
+    result: "BinPacking2DAlgo.Algo" = BinPacking2DAlgo.single_run(input_data, MATERIAL_SIZE,
+                                                                  parameter_input_array=arg.weights,
+                                                                  algo_type=arg.algo_name,)
+    value = result.get_avg_util_rate()
+
+    return value
+
+def sub_process_pop_eval(arg:DE_MultiArgs):
+    # print(arg)
+    idxs = [idx for idx in range(arg.pop_size) if idx != arg.idvl_idx]
+    a, b, c = arg.pop[np.random.choice(idxs, 3, replace=False)]
+    mutant = a + np.random.uniform(arg.mutation, 1) * (b - c)
+    mutant = np.where(mutant < 0, 0, mutant)
+    mutant = np.where(mutant > 1, 1, mutant)
+    cross_points = np.random.rand(arg.dimensions) < arg.crossover
+    if not np.any(cross_points):
+        cross_points[np.random.randint(0, arg.dimensions)] = True
+    trial = np.where(cross_points, mutant, arg.pop[arg.idvl_idx])
+    trial_denorm = arg.min_b + trial * arg.diff
+    trial_denorm = np.where(trial_denorm < arg.min_b, arg.min_b, trial_denorm)
+    trial_denorm = np.where(mutant > arg.max_b, arg.max_b, trial_denorm)
+    data_set = data_sets[arg.data_set_name]
+    input_data = [ kde_sample(data_set,arg.data_sample_scale)
+                   if arg.random_ratio is None
+                   else random_mix(kde_sample(data_set,arg.data_sample_scale)[:,1:],arg.random_ratio)
+                   for k in range(arg.eval_run_count)]
+    results = BinPacking2DAlgo.multi_run(input_data,MATERIAL_SIZE,parameter_input_array=trial_denorm,algo_type=arg.algo_name,run_count=arg.eval_run_count)
+    fitness = np.mean(1/np.array(results))
+    for obj in list(locals().keys())[:]:
+        if not (locals()[obj]  is  fitness or locals()[obj]  is  trial_denorm or locals()[obj]  is  trial or locals()[obj]  is  arg.idvl_idx):
+            del locals()[obj]
+    gc.collect()
+    return fitness,trial_denorm,trial,arg.idvl_idx
 
 class DE:
-    def __init__(self,p:Pool, data_set, data_set_name, eval_selector=EvalSelect.Single, pop_size=20, eval_run_count=40,
+    def __init__(self,p:multiprocessing.Pool, data_set, data_set_name, eval_selector=EvalSelect.Single, pop_size=20, eval_run_count=40,
                  data_sample_scale=1000, random_ratio=None, algo_name=AlgoName.Dist_MaxRect, max_iter=500,
                  ):
         """
@@ -234,7 +222,7 @@ class DE:
         min_b, max_b = np.asarray(self.bounds).T
         diff = np.fabs(min_b - max_b)
         pop_denorm = min_b + pop * diff
-        fitness = np.asarray([self.mutli_process_single_eval(ind) for ind in pop_denorm])
+        fitness = np.asarray([self.idvl_eval(ind) for ind in pop_denorm])
         best_idx = np.argmin(fitness)
         best = pop_denorm[best_idx]
         # 整体平均和历史最高
@@ -244,13 +232,13 @@ class DE:
         for i in range(self.max_iter):
 
             if len(history_best_fitness)>20 and np.var(history_best_fitness[-20:])<1e-7:
-                    print("\nrestart")
-                    best_avg_fitness = np.min(history_mean_fitness[-20:])
-                    for k in range(self.pop_size):
-                        if fitness[k]>best_avg_fitness:
-                            pop[k]=np.random.rand(1,dimensions)
-                            idvl_denorm=min_b+pop[k]*diff
-                            fitness[k]=self.mutli_process_single_eval(idvl_denorm)
+                print("\nrestart")
+                best_avg_fitness = np.min(history_mean_fitness[-20:])
+                for k in range(self.pop_size):
+                    if fitness[k]>best_avg_fitness:
+                        pop[k]=np.random.rand(1,dimensions)
+                        idvl_denorm=min_b+pop[k]*diff
+                        fitness[k]=self.idvl_eval(idvl_denorm)
             self.current_gen = i
             current_generation_fitness = []
             selected_indices = np.random.choice(range(self.pop_size), int(self.pop_size * np.random.uniform(0.7, 1)),
@@ -271,7 +259,7 @@ class DE:
                         cross_points[np.random.randint(0, dimensions)] = True
                     trial = np.where(cross_points, mutant, pop[j])
                     trial_denorm = min_b + trial * diff
-                    f = self.mutli_process_single_eval(trial_denorm)
+                    f = self.idvl_eval(trial_denorm)
                     current_generation_fitness.append(f)
                     if f < fitness[j]:
                         fitness[j] = f
@@ -283,7 +271,7 @@ class DE:
             else:# multi indvl run mode
 
                 input_env = [self.get_DE_multiArgs(j,pop,min_b,max_b,diff,fitness) for j in selected_indices]
-                results = self.p.map(self.multi_process_multi_eval, input_env)
+                results = self.p.map(sub_process_pop_eval, input_env)
                 for trial_f,trial_denorm,trial, idvl_idx in results:
                     current_generation_fitness.append(trial_f)
                     if trial_f < fitness[idvl_idx]:
@@ -299,7 +287,7 @@ class DE:
 
     def get_DE_multiArgs(self,idx,pop,min_b,max_b,diff,fitness):
         args = DE_MultiArgs(
-                data_set=self.data_set,
+                data_set_name=self.data_set_name,
                 idvl_idx=idx,
                 mutation=self.mutation,
                 crossover=self.crossover,
@@ -319,29 +307,6 @@ class DE:
 
 
 
-    @staticmethod
-    def multi_process_multi_eval(arg:DE_MultiArgs):
-        # print(arg)
-        idxs = [idx for idx in range(arg.pop_size) if idx != arg.idvl_idx]
-        a, b, c = arg.pop[np.random.choice(idxs, 3, replace=False)]
-        mutant = a + np.random.uniform(arg.mutation, 1) * (b - c)
-        mutant = np.where(mutant < 0, 0, mutant)
-        mutant = np.where(mutant > 1, 1, mutant)
-        cross_points = np.random.rand(arg.dimensions) < arg.crossover
-        if not np.any(cross_points):
-            cross_points[np.random.randint(0, arg.dimensions)] = True
-        trial = np.where(cross_points, mutant, arg.pop[arg.idvl_idx])
-        trial_denorm = arg.min_b + trial * arg.diff
-        trial_denorm = np.where(trial_denorm < arg.min_b, arg.min_b, trial_denorm)
-        trial_denorm = np.where(mutant > arg.max_b, arg.max_b, trial_denorm)
-        # print(a, b, c)
-        input_data = [ kde_sample(arg.data_set,arg.data_sample_scale) if arg.random_ratio is None else random_mix(kde_sample(arg.data_set,arg.data_sample_scale)[:,1:],arg.random_ratio)
-                       for k in range(arg.eval_run_count)]
-        results = BinPacking2DAlgo.multi_run(input_data,MATERIAL_SIZE,parameter_input_array=trial_denorm,algo_type=arg.algo_name,run_count=arg.eval_run_count)
-        fitness = np.mean(1/np.array(results))
-        gc.collect()
-        # show_memory()
-        return fitness,trial_denorm,trial,arg.idvl_idx
 
         pass
 
@@ -365,12 +330,17 @@ class DE:
         result = np.column_stack((range(self.data_sample_scale), result))
         return result
 
-    def mutli_process_single_eval(self, param: np.ndarray):
+    def idvl_eval(self, weights: np.ndarray):
         """ 这个代码用于专门执行单一的评估操作 用map来并行single_eval"""
         start = time()
 
-        datas = [AlgoArgs(self.get_sampled_items(), param, self.algo_name) for i in range(self.eval_run_count)]
-        result = self.p.map(self.single_eval, datas)
+        datas = [SingleAlgoArgs(weights=weights,
+                                algo_name=self.algo_name,
+                                data_sample_scale=self.data_sample_scale,
+                                data_set_name = self.data_set_name,
+                                random_ratio=self.random_ratio)
+                 for i in range(self.eval_run_count)]
+        result = self.p.map(sub_process_idvl_eval, datas)
         result = np.array(result)
         # print(result)
         mean = np.mean(result)
@@ -385,40 +355,31 @@ class DE:
         # show_memory()
         return 1 / mean
 
-    @staticmethod
-    def single_eval(arg: AlgoArgs):
-        start = time()
-        result: "BinPacking2DAlgo.Algo" = BinPacking2DAlgo.single_run(arg.data, MATERIAL_SIZE,
-                                                                      parameter_input_array=arg.params,
-                                                                      algo_type=arg.algo_name,)
-        value = result.get_avg_util_rate()
-        end = time()
-        # print(f"{round(end-start,2)}s,{round(value*100,2)}%",end=", ")
-        return value
 
 
 
 
-    def run_v1_eval(self,idvl_param):
-        input_data = [self.get_sampled_items()
-                      for k in range(self.eval_run_count)]
-        results = BinPacking2DAlgo.multi_run(input_data, MATERIAL_SIZE, parameter_input_array=idvl_param,
-                                             algo_type=self.algo_name, run_count=self.eval_run_count)
-        results = np.array(results)
-        return np.mean(1/results)
 
-    def callback(self, xk, convergence):
-        self.time_recorder.append(time())
-        eval_value = self.run_v1_eval(xk)
-        self.training_log.append([len(self.time_recorder), 1 / eval_value])
-        self.current_gen += 1
-        # if self.current_gen % 50 == 0:
-        #     np.save(f"at_gen_{self.current_gen}_" + self.log_save_name, np.array(self.training_log))
-        #     np.save(f"at_gen_{self.current_gen}_" + self.param_save_name(eval_value), xk)
-        print(
-            f'\ncurrent_gen={self.current_gen}, time cost {self.time_recorder[-1] - self.time_recorder[-2]} Current solution: {list(xk)}, ratio={1 / eval_value} , Convergence: {convergence}\n')
+    # def run_v1_eval(self,idvl_param):
+    #     input_data = [self.get_sampled_items()
+    #                   for k in range(self.eval_run_count)]
+    #     results = BinPacking2DAlgo.multi_run(input_data, MATERIAL_SIZE, parameter_input_array=idvl_param,
+    #                                          algo_type=self.algo_name, run_count=self.eval_run_count)
+    #     results = np.array(results)
+    #     return np.mean(1/results)
 
-        self.training_log.append([len(self.time_recorder), 1 / eval_value])
+    # def callback(self, xk, convergence):
+    #     self.time_recorder.append(time())
+    #     eval_value = self.run_v1_eval(xk)
+    #     self.training_log.append([len(self.time_recorder), 1 / eval_value])
+    #     self.current_gen += 1
+    #     # if self.current_gen % 50 == 0:
+    #     #     np.save(f"at_gen_{self.current_gen}_" + self.log_save_name, np.array(self.training_log))
+    #     #     np.save(f"at_gen_{self.current_gen}_" + self.param_save_name(eval_value), xk)
+    #     print(
+    #         f'\ncurrent_gen={self.current_gen}, time cost {self.time_recorder[-1] - self.time_recorder[-2]} Current solution: {list(xk)}, ratio={1 / eval_value} , Convergence: {convergence}\n')
+    #
+    #     self.training_log.append([len(self.time_recorder), 1 / eval_value])
 
 
 def solution_draw(solution: BinPacking2DAlgo.Algo, text=""):
@@ -447,7 +408,7 @@ class Training:
 
 
     def run(self):
-        with Pool() as p:
+        with multiprocessing.Pool() as p:
             start_time = time()
 
             result = []
@@ -457,7 +418,7 @@ class Training:
                         start_time2 = time()
                         print(training_type, name, algo_name, "start")
                         d = DE(p,data, name, random_ratio=(0, 0.3) if training_type == NOISED else None,
-                               eval_selector=EvalSelect.Single,max_iter=500,
+                               eval_selector=EvalSelect.Multi,max_iter=500,
                                algo_name=algo_name)
                         d.run_v2()
 
@@ -468,6 +429,7 @@ class Training:
 
 
 if __name__ == "__main__":
+    multiprocessing.set_start_method("spawn")
     t = Training([
         # [华为杯_data, PRODUCTION_DATA1],
         # [外包_data, PRODUCTION_DATA2],
