2b0f082e9ec56c539a67e7e8fb88927297ee2b64
a5642 <564298339@qq.com>
2023-11-15 01:35:15 +0800

    1.126

--- traning.py
+++ traning.py
@@ -136,27 +136,7 @@ class DE:
         self.log_save_name = f"traning_log_{(NOISED + '_') if self.random_ratio is not None else ''}_{self.data_set_name}_{self.algo_name}_{self.data_sample_scale}.npy"
         self.param_save_name = lambda \
             fun: f"{(NOISED + '_') if self.random_ratio is not None else ''}_{self.data_set_name}_param_{self.algo_name}_{self.data_sample_scale}_{round(fun, 2)}.npy"
-    # def run_v1(self):
-    #     start_time = time()
-    #     self.time_recorder = [start_time]
-    #     self.traning_log = []
-    #
-    #     de = DE_EVAL_for_run_v1(self.data_set,self.data_sample_scale,self.random_ratio,self.algo_name,self.eval_run_count)
-    #
-    #     result = differential_evolution(de.eval, self.bounds, workers=-1, atol=0.0001, strategy="randtobest1exp", popsize=20, callback=self.callback, maxiter=500)
-    #     end_time = time()
-    #     print(end_time - start_time)
-    #     to_save = [end_time, result.x, result.fun]
-    #     print(to_save)
-    #     np.save(f"{data_set_name}_traning_log__{round(end_time)}.npy", np.array(self.traning_log))
-    #     np.save(os.path.join(SYNC_PATH,
-    #                          f"{self.algo_name}_param_{NOISED if self.random_ratio is not None else STANDARD}_{self.data_set_name}_{self.data_sample_scale}_gen{self.max_iter}"),
-    #             result.x)
-    #     np.save(os.path.join(SYNC_PATH,
-    #                          f"{self.algo_name}_traininglog_{NOISED if self.random_ratio is not None else STANDARD}_{self.data_set_name}_{self.data_sample_scale}_gen{self.max_iter}"),
-    #             np.array(self.training_log))
-    #
-    #     return result.x, result.fun, self.traning_log
+
     def run_v2(self):
         self.time_recorder.append(time())
         # current_best = None
